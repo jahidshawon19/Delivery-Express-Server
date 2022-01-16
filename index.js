42bea2d8eff142ac023202542c7170e88389775a
jahidshawon19 <jahidshawon1730@gmail.com>
2022-01-16 14:41:27 +0600

    create put and get api for make admin

--- index.js
+++ index.js
@@ -64,6 +64,29 @@ async function run() {
         })
 
 
+        // PUT API FOR MAKE ADMIN 
+
+        app.put('/users/admin', async (req, res) =>{
+            const user = req.body 
+            const filter = {email: user.email}
+            const updateDoc = {$set: {role:'admin'}}
+            const result = await userCollection.updateOne(filter, updateDoc)
+            res.json(result)
+        })
+
+        app.get('/users/:email', async (req, res) =>{
+            const email = req.params.email 
+            const query = {email:email}
+            const user = await userCollection.findOne(query)
+            let isAdmin = false 
+            if(user?.role === 'admin'){
+                isAdmin=true
+            }
+            res.json({admin: isAdmin})
+        })
+  
+
+
     } finally {
         // await client.close()
     }
