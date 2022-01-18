4098002680c1e7708c67e1fab7e71c7d3a2275c7
jahidshawon19 <jahidshawon1730@gmail.com>
2022-01-18 13:31:48 +0600

    post and get api for add service, service details

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const { MongoClient } = require('mongodb');
+const { MongoClient} = require('mongodb');
 require('dotenv').config()
 const cors = require('cors')
-
+const ObjectId = require('mongodb').ObjectId
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -28,6 +28,7 @@ async function run() {
         const database = client.db('delivery_express')
         const bookingCollection = database.collection('booking')
         const userCollection = database.collection('users')
+        const serviceCollection = database.collection('services')
         
 
 
@@ -85,7 +86,29 @@ async function run() {
             res.json({admin: isAdmin})
         })
   
+        // POST API FOR ADDING SERVEICE 
 
+        app.post('/addservices', async (req, res)=>{
+            const newService = req.body 
+            const result = await serviceCollection.insertOne(newService)
+            res.json(result)
+        })
+
+        //  GET API FOR DISPLAY SERVICE 
+
+        app.get('/services', async (req, res)=>{
+            const cursor = serviceCollection.find({})
+            const allService = await cursor.toArray()
+            res.send(allService)
+        })
+
+        // GET API FOR GETTING SPECIFIC INFORMATION BY ID 
+        app.get('/service/:id', async(req, res)=>{
+            const id = req.params.id 
+            const query = {_id:ObjectId(id)}
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+        })
 
     } finally {
         // await client.close()
